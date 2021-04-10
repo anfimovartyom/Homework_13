8b57f627e738b37312af9f48e636407c9741e310
AnfimovArtyom <anfimovartyom@yandex.ru>
2021-04-10 13:43:47 +0300

    Добавьте файлы проекта.

---
+++ Homework_13.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+	//Присваиваем аргументам a,b значения.
+	int Result = SumSquared(2, 4);
+
+	//Выводим результат квадрата суммы аргументов.
+	std::cout << "Result = " << Result << std::endl;
+
+	return 0;
+}
